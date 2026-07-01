cfa9fd322d73306ba4927b264107e3a77a5dcc11
Anna <anna@MacBook-Air-Anna.local>
2026-07-01 18:55:47 +0300

    add layer combine tasks and solutions

---
+++ src/main/scala/ru/krivitskaya/anna/solutions/lesson10/AppLayers.scala
@@ -0,0 +1,66 @@
+package ru.krivitskaya.anna.solutions.lesson10
+
+import zio._
+
+case class Config(url: String, timeout: Int)
+
+case class DatabaseConnection(config: Config) {
+  def query(sql: String): UIO[String] =
+    ZIO.succeed(s"Result from db ${config.url} for sql $sql")
+}
+
+object DatabaseConnection {
+  val live: URLayer[Config, DatabaseConnection] = ZLayer.fromFunction(DatabaseConnection.apply _)
+}
+
+case class Cache(config: Config) {
+  def get(key: String): UIO[Option[String]] =
+    ZIO.some(s"Result from cache ${config.url} for key $key")
+}
+
+object Cache {
+  val live: URLayer[Config, Cache] = ZLayer.fromFunction(Cache.apply _)
+}
+
+case class UserService(db: DatabaseConnection, cache: Cache) {
+  def findUser(id: Int): UIO[String] =
+    for {
+      cached <- cache.get(s"user:$id")
+      result <- cached match {
+        case Some(v) => ZIO.succeed(v)
+        case None    => db.query(s"SELECT * FROM users WHERE id = $id")
+      }
+    } yield result
+}
+
+object UserService {
+  val live: URLayer[DatabaseConnection with Cache, UserService] =
+    ZLayer.fromFunction(UserService.apply _)
+}
+
+object AppLayers extends ZIOAppDefault {
+  val dbConfigLayer: ULayer[Config] =
+    ZLayer.succeed(Config("jdbc:postgresql://localhost:5432/mydb", 10000))
+
+  val cacheConfigLayer: ULayer[Config] =
+    ZLayer.succeed(Config("redis://admin:foobar@redis.server:6380", 100))
+
+  // Реализуйте ручную сборку через операторы >>> и ++
+  val manualLayer: ULayer[UserService] =
+    (dbConfigLayer >>> DatabaseConnection.live) ++ (cacheConfigLayer >>> Cache.live) >>> UserService.live
+
+  // Реализуйте автоматическую сборку, насколько это возможно, через ZLayer.make
+  val semiAutoLayer: ULayer[UserService] = ZLayer.make[UserService](
+    dbConfigLayer >>> DatabaseConnection.live,
+    cacheConfigLayer >>> Cache.live,
+    UserService.live
+  )
+
+  override def run =
+    for {
+      _ <- Console.printLine("=== Manual ===")
+      _ <- ZIO.serviceWithZIO[UserService](_.findUser(42).debug).provide(manualLayer.debug)
+      _ <- Console.printLine("=== Semi auto ===")
+      _ <- ZIO.serviceWithZIO[UserService](_.findUser(42).debug).provide(semiAutoLayer.debug)
+    } yield ()
+}
---
+++ src/main/scala/ru/krivitskaya/anna/solutions/lesson10/ComplicatedDeps.scala
@@ -0,0 +1,77 @@
+package ru.krivitskaya.anna.solutions.lesson10
+
+import zio._
+
+case class AppConfig(appName: String, connectionUrl: String, timeout: Int) {
+  println("AppConfig initialized")
+}
+
+case class Database(config: AppConfig) {
+  println("Database initialized")
+}
+
+case class FirstRepo(database: Database) {
+  println("FirstRepo initialized")
+}
+
+case class SecondRepo(database: Database) {
+  println("SecondRepo initialized")
+}
+
+case class FirstService(firstRepo: FirstRepo) {
+  println("FirstService initialized")
+}
+
+case class SecondService(secondRepo: SecondRepo) {
+  println("SecondService initialized")
+}
+
+case class App(config: AppConfig, firstService: FirstService, secondService: SecondService) {
+  println("App initialized")
+
+  def launchApp: UIO[Unit] = ZIO.logInfo(s"App named ${config.appName} is launched")
+}
+
+object ComplicatedDeps extends ZIOAppDefault {
+  val configLayer =
+    ZLayer.succeed(AppConfig("aboba", "jdbc:postgresql://localhost:5432/mydb", 1000))
+
+  val databaseLayer = ZLayer.fromFunction(Database.apply _)
+
+  val firstRepoLayer = ZLayer.fromFunction(FirstRepo.apply _)
+
+  val secondRepoLayer = ZLayer.fromFunction(SecondRepo.apply _)
+
+  val firstServiceLayer = ZLayer.fromFunction(FirstService.apply _)
+
+  val secondServiceLayer = ZLayer.fromFunction(SecondService.apply _)
+
+  val appLayer = ZLayer.fromFunction(App.apply _)
+
+  val dbCompletedLayer = configLayer >>> databaseLayer
+
+  val reposCompletedLayers = dbCompletedLayer >>> (firstRepoLayer ++ secondRepoLayer)
+
+  val servicesCompletedLayers = reposCompletedLayers >>> (firstServiceLayer ++ secondServiceLayer)
+
+  // Реализуйте ручную сборку через операторы >>> и ++
+  val manualLayersComplicated: ULayer[App] = (configLayer ++ servicesCompletedLayers) >>> appLayer
+
+  // Реализуйте ручную сборку через оператор >+>
+  val manualLayersAlternative: ULayer[App] =
+    configLayer >+>
+      databaseLayer >+>
+      firstRepoLayer >+>
+      secondRepoLayer >+>
+      firstServiceLayer >+>
+      secondServiceLayer >+>
+      appLayer
+
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
+    for {
+      _ <- Console.printLine("=== Manual ===")
+      _ <- ZIO.serviceWithZIO[App](_.launchApp).provide(manualLayersComplicated.debug)
+      _ <- Console.printLine("=== Manual alternative ===")
+      _ <- ZIO.serviceWithZIO[App](_.launchApp).provide(manualLayersAlternative.debug)
+    } yield ()
+}
